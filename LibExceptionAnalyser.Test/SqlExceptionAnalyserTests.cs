41d2d717e7ae52f0c7bd188162252a3f7faeb698
gitName <a@b.c>
2022-02-11 21:33:10 +0000

    feat: aded sqlException analyser

---
+++ LibExceptionAnalyser.Test/SqlExceptionAnalyserTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LibExceptionAnalyser.Test
+{
+    public class SqlExceptionAnalyserTests
+    {
+        [Fact]
+        public void should_return_all_sqlExceptions()
+        {
+
+        }
+    }
+}
